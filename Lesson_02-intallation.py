d8590de575e8dd5b65418d08b771b90450a2152c
mohdamirmir <47493839+mohdamirmir@users.noreply.github.com>
2024-01-03 00:52:12 +0400

    learning on 02-01-24

---
+++ Lesson_02-intallation.py
@@ -0,0 +1,5 @@
+#Lesson:02 - Python installation
+# check before installaton python --version and python3 --version
+# we need to use IDE - Integrated development environment
+# download from python.org
+# for ide u can use visual studio code or pycharm, install one of them.
