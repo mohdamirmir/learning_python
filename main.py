a5b50919b740daacff05916eb6fb24739326f9d8
mamir <mamir@noon.com>
2024-01-11 10:08:01 +0400

    09jan24

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-#Lesson:46 - polymorphism
+#Lesson:48 - Operator overloading
 '''
-one thing with multiple forms 
-''' 
+int is basically a class and you call add method on it
+'''
+a=5
+b=6
+print(int.__add__(5,6)) 
+print(a+b)
+
+c="aamir"
+d="mir"
+print(c+d)
+print(str.__add__('aamir','mir')) 
