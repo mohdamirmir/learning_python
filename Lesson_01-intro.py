adccf4b6afa7bdbf693b148db3bf81df48839c80
mohdamirmir <47493839+mohdamirmir@users.noreply.github.com>
2024-09-13 18:09:48 +0400

    Update Lesson_01-intro.py

--- Lesson_01-intro.py
+++ Lesson_01-intro.py
@@ -1,4 +1,4 @@
 # Lesson:01 - Introduction
 # Python is interpredted, object-orirented high level language
-# It is used by many companies liek google,reddit etc
+# It is used by many companies like google,reddit etc
 https://www.youtube.com/watch?v=QXeEoD0pB3E&list=PLsyeobzWxl7poL9JTVyndKe62ieoN-MZ3
