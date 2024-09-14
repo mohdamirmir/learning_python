b341dc7970fbd33c6d200fa285b51ac7c375d635
mohdamirmir <47493839+mohdamirmir@users.noreply.github.com>
2024-09-14 12:39:50 +0400

    Update Lesson_25_install_numpy.py

--- Lesson_25_install_numpy.py
+++ Lesson_25_install_numpy.py
@@ -11,5 +11,17 @@ from numpy import *
 arr = array([1,2,3,4,5,6])
 print(arr)
 
+
+matrix = array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+# Accessing elements
+print(matrix[0, 0])  # Output: 1
+print(matrix[2, 1])  # Output: 8
+
+# Modifying an element
+matrix[1, 2] = 10
+print(matrix[1, 2]) 
+
+
 #various ways for creating arrays using numpy
 # linspace(), logspace(), arange(), zeros(), ones()
